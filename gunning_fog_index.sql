6708900a66a91f414da1ba28aa6f5a6bfeb83469
lkrausi <lkraus73@googlemail.com>
2022-08-31 11:28:21 +0200

    update

--- gunning_fog_index.sql
+++ gunning_fog_index.sql
@@ -5,7 +5,10 @@ DROP TABLE IF EXISTS gunningfogindex;
 CREATE TABLE gunningfogindex(
 	kam_key INT NOT NULL,
     title VARCHAR(1000),
-    description_index FLOAT,
-    response_index FLOAT, 
-    conclusion_index FLOAT, 
+	gunning_fog_description_index FLOAT NULL, 
+    flesch_reading_description_index FLOAT NULL, 
+    gunning_fog_response_index FLOAT NULL, 
+    flesch_reading_response_index FLOAT NULL, 
+    gunning_fog_conclusion_index FLOAT NULL, 
+    flesch_reading_conclusion_index FLOAT NULL, 
     PRIMARY KEY (kam_key));
--- insert_data_into_mysql.py
+++ insert_data_into_mysql.py
@@ -1,3 +1,4 @@
+
 import textstat
 import text_analysis as ta
 import mysql.connector as mysql
@@ -14,22 +15,39 @@ def insert_data_in_mysql(text:list):
         if key_already_exists_in_database(text[i]['key']) == False:
             key = text[i]["key"]
             title = text[i]["title"]
-            description_index = textstat.gunning_fog(text[i]["description"])
-            if text[i]["response"] == None:
-                response_index = 0
+            gf_description_index = textstat.gunning_fog(text[i]["description"])
+            fre_description_index = textstat.flesch_reading_ease(text[i]["description"])
+            if text[i]["response"] != None:
+                gf_response_index = textstat.gunning_fog(text[i]["response"])
             else:
-                response_index = textstat.gunning_fog(text[i]["response"])  
-            if text[i]["conclusion"] == None:
-                conclusion_index = 0
+                gf_response_index = 0 
+            if text[i]["response"] != None:
+                fre_response_index = textstat.flesch_reading_ease(text[i]["response"])
             else:
-                conclusion_index = textstat.gunning_fog(text[i]["conclusion"])  
-                
-            val = f'"{key}", "{title}", "{description_index}", "{response_index}", "{conclusion_index}"'
-            sql = f"INSERT INTO gunningfogindex(kam_key, title, description_index, response_index, conclusion_index) \
-            VALUES({val});"
+                fre_response_index = 0     
+            if text[i]["conclusion"] != None:
+                gf_conclusion_index = textstat.gunning_fog(text[i]["conclusion"])
+            else:
+                gf_conclusion_index =  0
+            if text[i]["conclusion"] != None:
+                fre_conclusion_index = textstat.flesch_reading_ease(text[i]["conclusion"])
+            else:
+                fre_conclusion_index =  0    
+            val = f'"{key}", "{title}", "{gf_description_index}", "{fre_description_index}", "{gf_response_index}", "{fre_response_index}", "{gf_conclusion_index}", "{fre_conclusion_index}"'
+            sql = f"INSERT INTO gunningfogindex(kam_key, title, gunning_fog_description_index, flesch_reading_description_index, gunning_fog_response_index, flesch_reading_response_index, gunning_fog_conclusion_index, flesch_reading_conclusion_index) VALUES({val});"
+            update_gf_response = "UPDATE gunningfogindex SET gunning_fog_response_index = NULL WHERE gunning_fog_response_index  = 0;"
+            update_gf_conclusion = "UPDATE gunningfogindex SET gunning_fog_conclusion_index = NULL WHERE gunning_fog_conclusion_index  = 0;"
+            update_fre_response = "UPDATE gunningfogindex SET flesch_reading_response_index = NULL WHERE flesch_reading_response_index  = 0;"
+            update_fre_conclusion = "UPDATE gunningfogindex SET flesch_reading_conclusion_index = NULL WHERE flesch_reading_conclusion_index  = 0;"
             
             my_cursor.execute(sql)
+            my_cursor.execute(update_gf_response)
+            my_cursor.execute(update_gf_conclusion)
+            my_cursor.execute(update_fre_response)
+            my_cursor.execute(update_fre_conclusion)
             my_cursor.execute("COMMIT;")
+            
+            
       
         
 def key_already_exists_in_database(key:int):
@@ -42,5 +60,4 @@ def key_already_exists_in_database(key:int):
         
 if __name__ == "__main__":
     text = ta.reading_data_from_xlsx_file("Textanalyse.xlsx")
-    print(insert_data_in_mysql(text))
-        
+    print(insert_data_in_mysql(text))        
